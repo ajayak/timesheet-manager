49cd2d0a43d00d0ef02c1b2bdcf7e30ba461bdb7
Ajay Kumar <ajay.kumar@dporganizer.com>
2017-04-27 15:37:21 +0530

    added redux-form reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,13 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 import counter from './counter';
 
 const rootReducer = combineReducers({
   counter,
   router,
+  form: formReducer
 });
 
 export default rootReducer;
