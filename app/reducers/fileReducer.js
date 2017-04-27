b6210af846fe16b6a862dd6c56d774091adb5bbc
Ajay Kumar <ajay.kumar@dporganizer.com>
2017-04-27 17:45:42 +0530

    added reducer logic to extract row data from excel

--- app/reducers/fileReducer.js
+++ app/reducers/fileReducer.js
@@ -9,14 +9,29 @@ type actionType = {
   type: string
 };
 
-export default function counter(state, action: actionType) {
+function parseWorkbook(workbook) {
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const result = {};
+  Object.keys(sheet).forEach(key => {
+    const column = key.substr(1);
+    if (/^\d+$/.test(+column)) {
+      if (!result[column]) result[column] = [];
+      return result[column].push(sheet[key].v);
+    }
+  });
+  return result;
+}
+
+export default function counter(state = [], action: actionType) {
   switch (action.type) {
     case INCREMENT_COUNTER:
       return state + 1;
     case DECREMENT_COUNTER:
       return state - 1;
     case FILE_UPLOAD:
-      return action.payload;
+      const workbook = parseWorkbook(action.payload);
+      return workbook;
     default:
       return state;
   }
--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -5,12 +5,11 @@ import { createBrowserHistory } from 'history';
 import { routerMiddleware, push } from 'react-router-redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
-import * as counterActions from '../actions/counter';
-import type { counterStateType } from '../reducers/counter';
+import * as fileActions from '../actions/fileActions';
 
 const history = createBrowserHistory();
 
-const configureStore = (initialState: ?counterStateType) => {
+const configureStore = (initialState) => {
   // Redux Configuration
   const middleware = [];
   const enhancers = [];
@@ -31,7 +30,7 @@ const configureStore = (initialState: ?counterStateType) => {
 
   // Redux DevTools Configuration
   const actionCreators = {
-    ...counterActions,
+    ...fileActions,
     push,
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
