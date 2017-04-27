6ad07d16f7aa3f9e8d36cb5b5b262b828c9ac900
Ajay Kumar <ajay.kumar@dporganizer.com>
2017-04-27 14:58:17 +0530

    updated homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
+const info = require('../../package.json');
+
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
+          <h2>{info.productName}</h2>
+          <h3>{info.version}</h3>
           <Link to="/counter">to Counter</Link>
         </div>
       </div>
