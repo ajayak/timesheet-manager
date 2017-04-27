f6bf340cbe61f10c4ef9d1614d0481fb7ff5af08
Ajay Kumar <ajay.kumar@dporganizer.com>
2017-04-27 15:32:35 +0530

    renamed counter to home

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,13 @@ import { HashRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import mainPage from './containers/MainPage';
 
 export default () => (
   <Router>
     <App>
       <Switch>
-        <Route path="/main" component={CounterPage} />
+        <Route path="/main" component={mainPage} />
         <Route path="/" component={HomePage} />
       </Switch>
     </App>
